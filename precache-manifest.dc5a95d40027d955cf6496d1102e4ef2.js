f9968a10763b0cd8d1a45a78e13949d27a5616d2
cclulu <cclulu11@gmail.com>
2020-06-12 14:05:36 -0700

    Updates

---
+++ precache-manifest.dc5a95d40027d955cf6496d1102e4ef2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9069bf7d4b1bc7a6db93a1cec342df11",
+    "url": "/osoriobros/index.html"
+  },
+  {
+    "revision": "82b75ffb721455294f12",
+    "url": "/osoriobros/static/css/main.c28861f8.chunk.css"
+  },
+  {
+    "revision": "29b6cc64d7cd6da690d0",
+    "url": "/osoriobros/static/js/2.474b6429.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/osoriobros/static/js/2.474b6429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b75ffb721455294f12",
+    "url": "/osoriobros/static/js/main.38ea680b.chunk.js"
+  },
+  {
+    "revision": "06406a3d88668a9417e3",
+    "url": "/osoriobros/static/js/runtime-main.2310dbbd.js"
+  },
+  {
+    "revision": "2f9f9d2cf27b4b8f1bbe6d31bc16ce18",
+    "url": "/osoriobros/static/media/under-construction-4010445_1920.2f9f9d2c.jpg"
+  }
+]);
